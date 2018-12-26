6b8213a78e2c34790cb94efe36a565243b5986b1
shimo <zhuiyaoyan@126.com>
2018-12-26 18:05:38 +0800

    update

--- src/test/java/cases/docSheet/testDocHeader.java
+++ src/test/java/cases/docSheet/testDocHeader.java
@@ -72,7 +72,7 @@ public class testDocHeader extends TestInit {
      * @author 刘晨
      * @Time 2018-12-20
      */
-    @Test(enabled = true)
+    @Test(enabled = false)
     public void doc_createDoc() {
         login("testDocHeader@shimo.im", "123123");
 
@@ -80,8 +80,87 @@ public class testDocHeader extends TestInit {
         click(desktop1_1);
         click(b_headerCreate);
         click(b_headerCreateDoc);
+        //Collection_OK
 
-        Boolean r1 = driver.getCurrentUrl().equals(getURL() + "dashboard/updated");
+    }
+
+    /**
+     * 文档编辑页，header 收藏文件
+     *
+     * @author 刘晨
+     * @Time 2018-12-26
+     */
+    @Test(enabled = true)
+    public void doc_collectionDoc() {
+        login("testDocHeader@shimo.im", "123123");
+
+        click(desktop);
+        click(desktop1_1);
+        click(Collection_OK);
+
+        String toastMsg = getText(toast_msg);
+
+        Boolean r1 = toastMsg.endsWith("「testDocHeader」已取消收藏");
+        Boolean r2 = toastMsg.endsWith("「testDocHeader」已收藏");
+
+        assertTrue(r1 || r2);
+    }
+
+    /**
+     * 文档编辑页，header 点击用户头像展开用户菜单
+     *
+     * @author 刘晨
+     * @Time 2018-12-26
+     */
+    @Test(enabled = true)
+    public void doc_click_userIcon() {
+        login("testDocHeader@shimo.im", "123123");
+
+        click(desktop);
+        click(desktop1_1);
+        click(b_user_icon);
+
+        String result2 = getText(desktop_user_icon_status_L);
+        assertEquals(result2, "基础版");
+
+        String result3 = getText(desktop_user_icon_status_R);
+        assertEquals(result3, "升级到企业版或高级版");
+
+    }
+
+    /**
+     * 文档编辑页，header 点击讨论按钮
+     *
+     * @author 刘晨
+     * @Time 2018-12-26
+     */
+    @Test(enabled = true)
+    public void doc_click_discord() throws InterruptedException {
+        login("testDocHeader@shimo.im", "123123");
+
+        click(desktop);
+        click(desktop1_1);
+        click(b_discord);
+
+        Thread.sleep(500);
+        Boolean r1 = driver.findElement(By.id("discordpanel-show")).isDisplayed();
         assertTrue(r1);
+    }
+
+    /**
+     * 文档编辑页，header 点击演示按钮
+     *
+     * @author 刘晨
+     * @Time 2018-12-26
+     */
+    @Test(enabled = true)
+    public void doc_click_screenShow() {
+        login("testDocHeader@shimo.im", "123123");
+
+        click(desktop);
+        click(desktop1_1);
+        click(b_screenShow);
+        click(b_demoScreenBack);
+        click(b_back);
     }
 }
