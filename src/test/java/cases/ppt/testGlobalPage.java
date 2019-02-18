21789fdf3961f0ae30ec87f02bc83e20348ff2cd
W55555 <1350334784@qq.com>
2019-02-18 18:41:09 +0800

    update

--- src/test/java/cases/ppt/testGlobalPage.java
+++ src/test/java/cases/ppt/testGlobalPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class testGlobalPage extends TestInit {// 全局页面
@@ -17,7 +18,7 @@ public class testGlobalPage extends TestInit {// 全局页面
 	 *
 	 */
 	@Test(enabled = true)
-	public void New_Page() throws InterruptedException {
+	public void New_Page() {
 		login("New_Page@shimo.im", "123123");
 		click(desktop_new);
 		click(desktop_newslides);
@@ -37,7 +38,7 @@ public class testGlobalPage extends TestInit {// 全局页面
 	 *
 	 */
 	@Test(enabled = true)
-	public void New_Template() throws InterruptedException {
+	public void New_Template() {
 		login("NewTemplate@shimo.im", "123123");
 		click(desktop_new);
 		click(desktop_newslides);
@@ -61,7 +62,7 @@ public class testGlobalPage extends TestInit {// 全局页面
 	 *
 	 */
 	@Test(enabled = true)
-	public void Remove_Pages() throws InterruptedException {
+	public void Remove_Pages() {
 		login("Addpages@shimo.im", "123123");
 		click(desktop);
 		click(desktop1_1);
@@ -81,7 +82,7 @@ public class testGlobalPage extends TestInit {// 全局页面
 	 *
 	 */
 	@Test(enabled = true)
-	public void Remove_Pages_1() throws InterruptedException {
+	public void Remove_Pages_1() {
 		login("Addpages@shimo.im", "123123");
 		click(desktop);
 		click(desktop1_2);
@@ -89,6 +90,7 @@ public class testGlobalPage extends TestInit {// 全局页面
 		click(ppt_page_1);
 		action.sendKeys(Keys.DELETE).perform();
 		Boolean result1 = doesWebElementExist(page_elements_2);
+		click(ppt_revoke);
 		click(ppt_revoke);
 		Boolean result2 = doesWebElementExist(page_elements_2);
 		assertTrue(result1 && !result2);
@@ -102,25 +104,25 @@ public class testGlobalPage extends TestInit {// 全局页面
 	 *
 	 */
 	@Test(enabled = true)
-	public void Ppt_Right_1() throws InterruptedException {
+	public void Ppt_Right_1() {
 		login("ppt_right_1@shimo.im", "123123");
 		click(desktop);
 		click(desktop1_1);
 		contextClick(ppt_page_1);
-		click(right_key_new);
+		click(replication_pasting);
 		Boolean result1 = doesWebElementExist(page_elements_4);
 		action.sendKeys(Keys.DELETE).perform();
 		assertTrue(result1);
 	}
 	/**
-	 * 幻灯片中删除唯一页面
+	 * 右键新建幻灯片
 	 *
 	 * @author 王继程
 	 * @Time 2018-07-23
 	 *
 	 */
 	@Test(enabled = true)
-	public void Ppt_Right_2() throws InterruptedException {
+	public void Ppt_Right_2() {
 		login("ppt_right_2@shimo.im", "123123");
 		click(desktop);
 		click(desktop1_1);
@@ -130,7 +132,24 @@ public class testGlobalPage extends TestInit {// 全局页面
 		action.sendKeys(Keys.DELETE).perform();
 		assertTrue(result1);
 	}
-
+	/**
+	 * 右键删除幻灯片
+	 *
+	 * @author 王继程
+	 * @Time 2018-07-23
+	 *
+	 */
+	@Test(enabled = true)
+	public void Ppt_Right_3() {
+		login("ppt_right_3@shimo.im", "123123");
+		click(desktop);
+		click(desktop1_1);
+		contextClick(ppt_page_2);
+		click(delete_page);
+		Boolean result1 = doesWebElementExist(ppt_page_2);
+		click(ppt_revoke);
+		assertFalse(result1);
+	}
 
 
 
@@ -148,7 +167,7 @@ public class testGlobalPage extends TestInit {// 全局页面
 	 *
 	 */
 	@Test(enabled = true)
-	public void Play_1() throws InterruptedException {
+	public void Play_1() {
 		login("Play_1@shimo.im", "123123");
 		click(desktop);
 		click(desktop1_1);
@@ -187,7 +206,7 @@ public class testGlobalPage extends TestInit {// 全局页面
 	 * @Time 2018-07-23 false
 	 */
 	@Test(enabled = false)
-	public void Sorting_Order() throws InterruptedException {
+	public void Sorting_Order() {
 		login("SortingOrder@shimo.im", "123123");
 		click(desktop);
 		click(desktop1_1);
