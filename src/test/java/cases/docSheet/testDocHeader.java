c842039bb815214c17ca1ef8138f069d718c7bf5
shimo <zhuiyaoyan@126.com>
2019-02-28 17:02:52 +0800

    update

--- src/test/java/cases/docSheet/testDocHeader.java
+++ src/test/java/cases/docSheet/testDocHeader.java
@@ -2,7 +2,6 @@ package cases.docSheet;
 
 import base.TestInit;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
