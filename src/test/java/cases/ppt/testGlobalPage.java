cb17cd3416be9378cd2bf7d42d6ffdeb92dac37f
W55555 <1350334784@qq.com>
2019-03-13 14:31:45 +0800

    update

--- src/test/java/cases/ppt/testGlobalPage.java
+++ src/test/java/cases/ppt/testGlobalPage.java
@@ -102,7 +102,7 @@ public class testGlobalPage extends TestInit {// 全局页面
 	 * @Time 2018-07-23
 	 *
 	 */
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void Ppt_Right_1() {
 		login("ppt_right_1@shimo.im", "123123");
 		click(desktop);
