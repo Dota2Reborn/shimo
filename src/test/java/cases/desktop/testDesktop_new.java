73a04f7c6cd069f020ccc2bcdd56cea4bd5fceec
shimo <zhuiyaoyan@126.com>
2019-06-24 16:02:57 +0800

    update

--- src/test/java/cases/desktop/testDesktop_new.java
+++ src/test/java/cases/desktop/testDesktop_new.java
@@ -103,7 +103,7 @@ public class testDesktop_new extends TestInit {
      * @author 刘晨
      * @Time 2017-11-20
      */
-    @Test(enabled = true)
+    @Test(enabled = false)
     public void desktop_import_doc() throws InterruptedException {
         login("autoTest_new@shimo.im", "123123");
         click(desktop);
@@ -133,7 +133,7 @@ public class testDesktop_new extends TestInit {
      * @author 刘晨
      * @Time 2017-11-20
      */
-    @Test(enabled = true)
+    @Test(enabled = false)
     public void desktop_import_docx() throws InterruptedException {
         login("autoTest_new@shimo.im", "123123");
         click(desktop);
