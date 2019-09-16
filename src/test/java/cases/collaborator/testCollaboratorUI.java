a5c914a5cd761e415d4d190bfc906777b98ab894
shimo <zhuiyaoyan@126.com>
2019-09-16 16:11:20 +0800

    update

--- src/test/java/cases/collaborator/testCollaboratorUI.java
+++ src/test/java/cases/collaborator/testCollaboratorUI.java
@@ -36,7 +36,7 @@ public class testCollaboratorUI extends TestInit {
      * @author 刘晨
      * @Time 2018-8-29
      */
-    @Test(enabled = false)
+    @Test(enabled = true)
     public void collaboratorUI_2() {
         login("panpan@panpan.com", "123123");
         click(desktop);
