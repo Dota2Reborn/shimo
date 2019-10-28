5d450c329f66fc9bb8b96217dc95fec91a055fec
shimo <zhuiyaoyan@126.com>
2019-10-28 14:53:30 +0800

    update

--- src/test/java/cases/dashboard/dashboard_update.java
+++ src/test/java/cases/dashboard/dashboard_update.java
@@ -136,7 +136,7 @@ public class dashboard_update extends TestInit {
         String text1 = getText(dashboard_update_time);
         click(dashboard_update_time);
         click(b_back);
-//        assertEquals(text1, "1 评论刚刚 cc 评论：通过");
+        System.out.println(text1);
         assertTrue(text1.contains("刚刚 cc 更新"));
 
     }
